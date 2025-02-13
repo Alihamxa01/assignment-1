ca9b4217a51ebe265c7d0eece678daaf7bfd421b
ALI HAMZA NADEEM RANA <159229677+Alihamxa01@users.noreply.github.com>
2025-02-13 21:53:34 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Page() {
     <div >
       <Header />
       <div id="home" className="flex pl-5 flex-col items-start justify-center min-h-screen font-black">
-        <h1 className="text-left pl-7 text-7xl">HEY! I AM</h1> {/* Align text to the left */}
-        <h1 className="mt-0 text-left text-6xl sm:text-9xl text-emerald-950">ALI HAMZA</h1> {/* Ensure text is left aligned */}
+        <h1 className="text-left  text-7xl">HEY! I AM</h1> 
+        <h1 className="mt-0 text-left text-7xl sm:text-9xl text-emerald-950">ALI HAMZA</h1> 
       <p className="font-serif text-2xl mt-5">Welcome to my portfolio website! <br/> Here, I will showcase my completed work, projects, and more.
 
 </p>
